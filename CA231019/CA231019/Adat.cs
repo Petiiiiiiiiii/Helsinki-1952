60e37cf9b93f481d5587c7992a8667d84642ae5f
Orosz Péter <70895314+Petiiiiiiiiii@users.noreply.github.com>
2023-10-19 09:13:13 +0200

    Add files via upload

---
+++ CA231019/CA231019/Adat.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA231019
+{
+    class Adat
+    {
+        public int Helyezes { get; set; }
+        public int DarabSportolo { get; set; }
+        public string Sportag { get; set; }
+        public string Versenyszam { get; set; }
+        public int OsszPontszam { get; set; }
+
+        public Adat(string sor)
+        {
+            var atmeneti = sor.Split(' ');
+            this.Helyezes = int.Parse(atmeneti[0]);
+            this.DarabSportolo = int.Parse(atmeneti[1]);
+            this.Sportag = atmeneti[2];
+            this.Versenyszam = atmeneti[3];
+            this.OsszPontszam = 0;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Helyezes} {this.DarabSportolo} {this.OsszPontszam} {this.Sportag} {this.Versenyszam}";
+        }
+    }
+}
---
+++ CA231019/CA231019/Program.cs
@@ -0,0 +1,137 @@
+﻿using CA231019;
+using System.Text;
+
+
+var sr = new StreamReader(
+    path:@"..\..\..\src\helsinki.txt",
+    encoding: Encoding.UTF8);
+
+var adatok = new List<Adat>();
+
+while (!sr.EndOfStream)
+    adatok.Add(new Adat(sr.ReadLine()));
+
+var f3 = adatok
+    .Count();
+
+Console.WriteLine("3. feladat: ");
+Console.WriteLine($"Pontszerző helyezések száma: {f3}");
+
+var f4dic = new Dictionary<string, int>();
+var f4 = adatok
+    .Where(a => a.Helyezes <= 3)
+    .GroupBy(g => g.Helyezes);
+
+foreach (var item in f4)
+{
+    if (item.Key == 1)
+        f4dic.Add("Arany", item.Count());
+    else if (item.Key == 2)
+        f4dic.Add("Ezüst", item.Count());
+    else
+        f4dic.Add("Bronz", item.Count());
+
+}
+
+Console.WriteLine("4. feladat:");
+foreach (var item in f4dic) 
+{
+    Console.WriteLine($"{item.Key}: {item.Value}");
+}
+
+var f4ossz = adatok
+    .Where(a => a.Helyezes <= 3);
+
+Console.WriteLine($"Összesen: {f4ossz.Count()}");
+
+
+var f5dic = new Dictionary<int, int>() { {1,7},{2,5},{3,4},{4,3},{5,2},{6,1} };
+var ossz = 0;
+
+foreach (var item in adatok)
+{
+    foreach (var pont in f5dic)
+    {
+        if (item.Helyezes == pont.Key)
+        {
+            ossz += pont.Value;
+        }
+    }
+}
+Console.WriteLine("5. feladat:");
+Console.WriteLine($"Olimpiai pontok száma: {ossz}");
+
+var f6Torna = adatok
+    .Where(a => a.Sportag == "torna")
+    .ToList();
+
+var f6Uszas = adatok
+    .Where(a => a.Sportag == "uszas")
+    .ToList();
+
+Console.WriteLine("6. feladat:");
+
+if (f6Torna.Count() > f6Uszas.Count())
+    Console.WriteLine("Torna sportágban szereztek többet.");
+else if (f6Uszas.Count() > f6Torna.Count())
+    Console.WriteLine("Úszás sportágban szereztek többet.");
+else
+    Console.WriteLine("Egyenlő volt az érmek száma");
+
+foreach (var item in adatok)
+{
+    foreach (var pont in f5dic)
+    {
+        if (item.Helyezes == pont.Key)
+        {
+            item.OsszPontszam = pont.Value;
+        }
+    }
+}
+
+foreach (var item in adatok) 
+{
+    if (item.Sportag == "kajakkenu")
+    {
+        item.Sportag = "kajak-kenu";
+    }
+}
+
+var sw = new StreamWriter(
+    path:@"..\..\..\src\helsinki2.txt",
+    append:false);
+
+foreach (var item in adatok)
+{
+    sw.WriteLine(item);
+}
+
+sw.Close();
+
+var f8 = adatok
+    .Where(a => a.DarabSportolo == adatok.Max(m => m.DarabSportolo));
+
+Console.WriteLine("8. feladat:");
+
+foreach (var item in f8) 
+{
+    Console.WriteLine($"Helyezés: {item.Helyezes}");
+    Console.WriteLine($"Sportág: {item.Sportag}");
+    Console.WriteLine($"Versenyszám: {item.Versenyszam}");
+    Console.WriteLine($"Sporotlók száma: {item.DarabSportolo}");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Console.ReadKey();
